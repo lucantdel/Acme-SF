529b9679daa9ace46ec077530fde5d2e136f9a62
alvvc02 <100714201+alvvc02@users.noreply.github.com>
2024-02-24 23:02:32 +0100

    fix: add Temporal a author of Notice

--- src/main/java/acme/entities/notices/Notice.java
+++ src/main/java/acme/entities/notices/Notice.java
@@ -4,13 +4,15 @@ package acme.entities.notices;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
@@ -30,19 +32,20 @@ public class Notice extends AbstractEntity {
 
 	@Past
 	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date				instantiationMoment;
 
 	@NotBlank
-	@Size(max = 75)
+	@Length(max = 76)
 	private String				title;
 
 	@NotBlank
-	@Size(max = 75)
+	@Length(max = 76)
 	@Pattern(regexp = "^\\w+ - \\w+, \\w+$")
 	private String				author;
 
 	@NotBlank
-	@Size(max = 100)
+	@Length(max = 101)
 	private String				message;
 
 	@Email
@@ -56,6 +59,7 @@ public class Notice extends AbstractEntity {
 	// Relationships ----------------------------------------------------------
 
 	//	// Constructor ----------------------------------------------------------
+	// He pensado en hacer un constructor para aplicar la regla de negocio de author mientras no tenemos service
 	//	public Notice(@Past final Date instantiationMoment, @NotBlank final String title, final String username, final String surname, final String name, @NotBlank final String message) {
 	//		this.instantiationMoment = instantiationMoment;
 	//		this.title = title;
