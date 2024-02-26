f6b1f2f4faf71449d396bd50b6178716111bb78e
[[mohanadabulatifa]] <mohanad.abulatifa@gmail.com>
2024-02-26 19:15:10 +0100

    feat:add objective entity

---
+++ src/main/java/acme/entities/objectives/Objective.java
@@ -0,0 +1,57 @@
+
+package acme.entities.objectives;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Objective extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+
+	public enum Priority {
+		Low, Medium, High
+	}
+
+
+	@Enumerated(EnumType.STRING)
+	private Priority	priority;
+
+	private boolean		critical;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date		startMoment;
+
+	@URL
+	private String		optionalLink;
+
+}
