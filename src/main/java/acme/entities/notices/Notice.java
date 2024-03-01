b084d7038aba6cd4cb8187848027229cb299376c
alvvc02 <100714201+alvvc02@users.noreply.github.com>
2024-03-01 20:35:54 +0100

    Fix: fix the length limit

--- src/main/java/acme/entities/notices/Notice.java
+++ src/main/java/acme/entities/notices/Notice.java
@@ -36,16 +36,16 @@ public class Notice extends AbstractEntity {
 	private Date				instantiationMoment;
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75)
 	private String				title;
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75)
 	@Pattern(regexp = "^\\w+ - \\w+, \\w+$")
 	private String				author;
 
 	@NotBlank
-	@Length(max = 101)
+	@Length(max = 100)
 	private String				message;
 
 	@Email
