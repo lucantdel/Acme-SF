92952de32312963f9e9a38cbb5ce8075f0bf1430
Manuel Orta <manortper1@alum.us.es>
2024-03-03 14:56:45 +0100

    feat: added auditor dashboard

---
+++ src/main/java/acme/forms/AuditorDashboard.java
@@ -0,0 +1,30 @@
+
+package acme.forms;
+
+import java.time.Duration;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuditorDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+	// Attributes -------------------------------------------------------------
+
+	Integer						totalNumberOfCodeAuditsStatic;
+	Integer						totalNumberOfCodeAuditsDinamic;
+	Duration					avegageTimeOfThePeriodLenght;
+	Duration					deviationTimeOfThePeriodLenght;
+	Duration					minimunTimeOfThePeriodLenght;
+	Duration					maximumTimeOfThePeriodLenght;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
