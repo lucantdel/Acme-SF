ab8ca95c23a4a8f84e7dffa03fe3cac9c2b903ca
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-03-04 00:02:29 +0100

    feat: add user story entity

---
+++ src/main/java/acme/entities/userstories/UserStory.java
@@ -0,0 +1,57 @@
+
+package acme.entities.userstories;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserStory extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			description;
+
+	@NotNull
+	@Min(1)
+	protected Integer			estimatedCost;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			acceptanceCriteria;
+
+	protected Priority			priority;
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@ManyToOne
+	protected Project			project;
+
+}
