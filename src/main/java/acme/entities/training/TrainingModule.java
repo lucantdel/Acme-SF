2eb4f481fe5a1f592352b00b93d3a648c810f03e
JCLPV <80278014+JCLPV@users.noreply.github.com>
2024-03-05 21:16:23 +0100

    feat: added Training Module Entity plus the Difficulty enumerator

---
+++ src/main/java/acme/entities/training/TrainingModule.java
@@ -0,0 +1,63 @@
+
+package acme.entities.training;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrainingModule extends AbstractEntity {
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@NotBlank
+	@Column(unique = true)
+	private String				code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				creationMoment;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				details;
+
+	private Difficulty			difficultyLevel;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				updateMoment;
+
+	@URL
+	private String				link;
+
+	@Min(0)
+	private Double				totalTime;
+
+	// Derived attributes ----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
