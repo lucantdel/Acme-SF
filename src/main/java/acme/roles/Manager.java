0af11fa85bef68a059b7ccc2929cfb37eb5a98c2
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-03-06 11:33:18 +0100

    fix: change private attributes to protected

--- src/main/java/acme/roles/Manager.java
+++ src/main/java/acme/roles/Manager.java
@@ -18,23 +18,23 @@ public class Manager extends AbstractRole {
 
 	// Serialisation identifier -----------------------------------------------
 
-	private static final long	serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
 	@Length(max = 75)
-	private String				degree;
+	protected String			degree;
 
 	@NotBlank
 	@Length(max = 100)
-	private String				overview;
+	protected String			overview;
 
 	@NotBlank
 	@Length(max = 100)
-	private String				certifications;
+	protected String			certifications;
 
 	@URL
-	private String				link;
+	protected String			link;
 
 }
