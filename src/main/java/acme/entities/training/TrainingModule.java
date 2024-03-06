8cfed22c5b05184696a9dc8a12662cc790a09570
JCLPV <80278014+JCLPV@users.noreply.github.com>
2024-03-06 18:56:46 +0100

    fix: temporarily remove totalTime
    To avoid future problems when mergeing do to the fact that it is a Derived atribute that depends on TrainingSession which still isnt implemented.

--- src/main/java/acme/entities/training/TrainingModule.java
+++ src/main/java/acme/entities/training/TrainingModule.java
@@ -9,7 +9,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -55,9 +54,6 @@ public class TrainingModule extends AbstractEntity {
 
 	@URL
 	private String				link;
-
-	@Min(0)
-	private Integer				totalTime;
 
 	// Derived attributes ----------------------------------------------------
 
