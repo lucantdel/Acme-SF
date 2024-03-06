8725da3f9dd3f37c70a16b4659175d5092dc9adf
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-03-06 11:32:38 +0100

    feat: add manager relationships

--- src/main/java/acme/entities/projects/UserStory.java
+++ src/main/java/acme/entities/projects/UserStory.java
@@ -2,6 +2,8 @@
 package acme.entities.projects;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -10,6 +12,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.roles.Manager;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -48,5 +51,10 @@ public class UserStory extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne
+	protected Manager			manager;
 
 }
