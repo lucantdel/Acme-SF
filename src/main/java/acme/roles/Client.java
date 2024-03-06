e508f11ad912e4bfc31eedd44503eb6b9cf17502
[[mohanadabulatifa]] <mohanad.abulatifa@gmail.com>
2024-03-06 02:10:52 +0100

    feat: client rol entity created

---
+++ src/main/java/acme/roles/Client.java
@@ -0,0 +1,46 @@
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Client extends AbstractRole {
+
+    private static final long serialVersionUID = 1L;
+
+    // Attributes -------------------------------------------------------------
+
+    // Identification with pattern "CLI-[0-9]{4}"
+    @Pattern(regexp = "CLI-[0-9]{4}", message = "Identification must match pattern CLI-[0-9]{4}")
+    @NotBlank
+    private String identification;
+
+    // Company name
+    @NotBlank
+    @Length(max = 75)
+    private String companyName;
+
+    // Type (either "company" or "individual")
+    @NotBlank
+    private String type;
+
+    // Email
+    @Email
+    @NotBlank
+    private String email;
+
+    @URL
+    private String optionalLink;
+
+}
