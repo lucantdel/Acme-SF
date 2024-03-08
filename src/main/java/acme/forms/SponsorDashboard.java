e5962362b6d3879b5d0a00ab3929e2ded6b05b63
alvvc02 <100714201+alvvc02@users.noreply.github.com>
2024-03-08 01:28:07 +0100

    fix: use of int instead of Map

--- src/main/java/acme/forms/SponsorDashboard.java
+++ src/main/java/acme/forms/SponsorDashboard.java
@@ -1,8 +1,6 @@
 
 package acme.forms;
 
-import java.util.Map;
-
 import acme.client.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,27 +10,26 @@ import lombok.Setter;
 public class SponsorDashboard extends AbstractForm {
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long		serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
 	// Número total de facturas con un impuesto menor o igual al 21.00%
-	// uso de Integer mejor
-	protected Map<String, Integer>	totalNumberOfInvoicesWithLowTax;
+	protected int				totalNumberOfInvoicesWithLowTax;
 
 	// Número total de patrocinios con un enlace
-	protected Map<String, Integer>	totalNumberOfSponsorshipsWithLink;
+	protected int				totalNumberOfSponsorshipsWithLink;
 
 	// Promedio, desviación, mínimo y máximo del monto de los patrocinios
-	protected double				avgSponsorshipsAmount;
-	protected double				devSponsorshipsAmount;
-	protected double				minSponsorshipsAmount;
-	protected double				maxSponsorshipsAmount;
+	protected double			avgSponsorshipsAmount;
+	protected double			devSponsorshipsAmount;
+	protected double			minSponsorshipsAmount;
+	protected double			maxSponsorshipsAmount;
 
 	// Promedio, desviación, mínimo y máximo de la cantidad de facturas
-	protected double				avgInvoicesQuantity;
-	protected double				devInvoicesQuantity;
-	protected double				minInvoicesQuantity;
-	protected double				maxInvoicesQuantity;
+	protected double			avgInvoicesQuantity;
+	protected double			devInvoicesQuantity;
+	protected double			minInvoicesQuantity;
+	protected double			maxInvoicesQuantity;
 
 }
