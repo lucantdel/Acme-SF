a076e0218d5361aef2a520b5c73ed1fc61aecdc1
[[mohanadabulatifa]] <mohanad.abulatifa@gmail.com>
2024-03-08 17:19:06 +0100

    feat: restrections and relations for progressLog  and role client added and fixed

--- src/main/java/acme/entities/progress_logs/ProgressLog.java
+++ src/main/java/acme/entities/progress_logs/ProgressLog.java
@@ -1,15 +1,24 @@
+
 package acme.entities.progress_logs;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Length;
+
 import acme.client.data.AbstractEntity;
+import acme.entities.contracts.Contract;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,23 +27,36 @@ import lombok.Setter;
 @Setter
 public class ProgressLog extends AbstractEntity {
 
-    private static final long serialVersionUID = 1L;
+	private static final long	serialVersionUID	= 1L;
 
-    @Pattern(regexp = "PG-[A-Z]{1,2}-[0-9]{4}", message = "Record id must match pattern PG-[A-Z]{1,2}-[0-9]{4}")
-    @NotBlank
-    private String recordId;
+	@NotNull
+	@Column(unique = true)
+	@Pattern(regexp = "PG-[A-Z]{1,2}-[0-9]{4}", message = "Record id must match pattern PG-[A-Z]{1,2}-[0-9]{4}")
+	@NotBlank
+	private String				recordId;
 
-    @NotNull
-    private double completenessPercentage;
+	@NotNull
+	@Min(1)
+	private double				completenessPercentage;
 
-    @NotBlank
-    private String progressComment;
+	@NotNull
+	@NotBlank
+	@Length(min = 0, max = 100)
+	private String				progressComment;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @NotNull
-    private Date registrationMoment;
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	private Date				registrationMoment;
 
-    @NotBlank
-    private String responsiblePerson;
+	@NotNull
+	@NotBlank
+	@Length(min = 0, max = 75)
+	private String				responsiblePerson;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Contract			contract;
 
 }
