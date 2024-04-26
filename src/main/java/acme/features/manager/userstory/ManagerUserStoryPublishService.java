7e623c9004043af015dc3b6a6e4b0e04840be970
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-04-26 22:28:08 +0200

    fix: userStory update and publish

--- src/main/java/acme/features/manager/userstory/ManagerUserStoryPublishService.java
+++ src/main/java/acme/features/manager/userstory/ManagerUserStoryPublishService.java
@@ -88,11 +88,20 @@ public class ManagerUserStoryPublishService extends AbstractService<Manager, Use
 
 		SelectChoices priorities;
 		Dataset dataset;
+		boolean isMine;
+		UserStory us;
+		Manager manager;
+
+		us = this.repository.findOneUserStoryById(super.getRequest().getData("id", int.class));
+		manager = this.repository.findOneManagerById(super.getRequest().getPrincipal().getActiveRoleId());
+
+		isMine = us.getManager().equals(manager);
 
 		priorities = SelectChoices.from(UserStoryPriority.class, object.getPriority());
 
 		dataset = super.unbind(object, "title", "description", "estimatedCost", "acceptanceCriteria", "priority", "link", "draftMode");
 		dataset.put("priorities", priorities);
+		dataset.put("isMine", isMine);
 
 		super.getResponse().addData(dataset);
 	}
