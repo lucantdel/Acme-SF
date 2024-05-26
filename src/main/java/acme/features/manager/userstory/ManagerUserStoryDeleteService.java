30d4bd03b6b01c192183ad0a214fc3d51cf694e8
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-05-26 18:15:26 +0200

    doc: add D04 docs

--- src/main/java/acme/features/manager/userstory/ManagerUserStoryDeleteService.java
+++ src/main/java/acme/features/manager/userstory/ManagerUserStoryDeleteService.java
@@ -86,11 +86,20 @@ public class ManagerUserStoryDeleteService extends AbstractService<Manager, User
 
 		SelectChoices priorities;
 		Dataset dataset;
+		boolean isMine;
+		UserStory us;
+		Manager manager;
+
+		us = this.repository.findOneUserStoryById(super.getRequest().getData("id", int.class));
+		manager = this.repository.findOneManagerById(super.getRequest().getPrincipal().getActiveRoleId());
+
+		isMine = us.getManager().equals(manager);
 
 		priorities = SelectChoices.from(UserStoryPriority.class, object.getPriority());
 
 		dataset = super.unbind(object, "title", "description", "estimatedCost", "acceptanceCriteria", "priority", "link", "draftMode");
 		dataset.put("priorities", priorities);
+		dataset.put("isMine", isMine);
 
 		super.getResponse().addData(dataset);
 	}
