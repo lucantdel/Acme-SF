0306099c2c6a8b257286cf08aadd09bd10cb2393
Lucas Antoñanzas del Villar <73561842+lucantdel@users.noreply.github.com>
2024-05-27 17:00:24 +0200

    feat: add indexes to manager entities

--- src/main/java/acme/entities/projects/Project.java
+++ src/main/java/acme/entities/projects/Project.java
@@ -3,7 +3,9 @@ package acme.entities.projects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -21,6 +23,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "manager_id"), @Index(columnList = "code")
+})
 public class Project extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
--- src/main/java/acme/entities/projects/ProjectUserStory.java
+++ src/main/java/acme/entities/projects/ProjectUserStory.java
@@ -2,7 +2,9 @@
 package acme.entities.projects;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -13,6 +15,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "project_id"), @Index(columnList = "user_story_id"), @Index(columnList = "project_id, user_story_id")
+})
 public class ProjectUserStory extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
