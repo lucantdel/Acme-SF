1f62675b37c7bd48e9e97a12be91f3105b87e084
alvvc02 <100714201+alvvc02@users.noreply.github.com>
2024-07-08 21:37:12 +0200

    fix: docs y group requirements

--- src/main/java/acme/entities/risks/Risk.java
+++ src/main/java/acme/entities/risks/Risk.java
@@ -10,6 +10,7 @@ import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
@@ -37,13 +38,16 @@ public class Risk extends AbstractEntity {
 	@Pattern(regexp = "R-[0-9]{3}")
 	private String				reference;
 
+	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	@Past
 	private Date				identificationDate;
 
+	@NotNull
 	@Min(0)
 	private Double				impact;
 
+	@NotNull
 	@Range(min = 0, max = 100)
 	private Double				probability;
 
