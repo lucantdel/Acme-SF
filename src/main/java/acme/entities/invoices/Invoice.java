87716e99d149e0b8242fc0624b7d962cc4fc34f9
alvvc02 <100714201+alvvc02@users.noreply.github.com>
2024-07-08 20:36:44 +0200

    fix: add docs and fix indices

--- src/main/java/acme/entities/invoices/Invoice.java
+++ src/main/java/acme/entities/invoices/Invoice.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
@@ -29,6 +31,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "sponsorship_id"), @Index(columnList = "code")
+})
 public class Invoice extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
--- src/main/java/acme/entities/sponsorships/Sponsorship.java
+++ src/main/java/acme/entities/sponsorships/Sponsorship.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -28,6 +30,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "sponsor_id"), @Index(columnList = "project_id"), @Index(columnList = "code")
+})
 public class Sponsorship extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
